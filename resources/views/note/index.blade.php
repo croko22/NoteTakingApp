412d29629b56a82eb89e1931dc2029a1568ef383
croko22 <kvchambit@gmail.com>
2024-06-12 13:33:09 -0500

    feat: Show tags on index note cards and show notes views

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,13 +1,33 @@
 <x-app-layout>
     <div class="py-12 note-container">
+        <form>
+            <div class="search">
+                <input type="search" class="search__input" placeholder="Search notes..." name="search"
+                    value="{{ request('search') }}">
+                <button class="search__button" type="submit">Search</button>
+            </div>
+        </form>
+        @foreach ($tags as $tag)
+            <a class="tag" href="{{ route('note.index', ['tag' => $tag->name]) }}"
+                class="tag">{{ $tag->name }}</a>
+        @endforeach
+
+
         <a href="{{ route('note.create') }}" class="new-note-btn">
             New Note
         </a>
+
+        @if (session('success'))
+            <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show" class="success-message">
+                {{ session('success') }}
+            </div>
+        @endif
+
         <div class="notes">
-            @foreach ($notes as $note)
-                <div class="note">
+            @forelse ($notes as $note)
+                <div class="note" href="{{ route('note.show', $note) }}">
                     <div class="note-header">
-                        <h3 class="px-2 py-4 text-2xl">Note: {{ $note->title }}</h3>
+                        <h3 class="px-2 py-4 text-2xl">{{ $note->name }}</h3>
                     </div>
                     <div class="note-body">
                         {{ Str::words($note->note, 30) }}
@@ -21,8 +41,13 @@
                             <button class="note-delete-button">Delete</button>
                         </form>
                     </div>
+                    @foreach ($note->tags as $tag)
+                        <span class="tag">{{ $tag->name }}</span>
+                    @endforeach
                 </div>
-            @endforeach
+            @empty
+                <p>No notes found</p>
+            @endforelse
         </div>
 
         <div class="p-6">
--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="note-container single-note">
         <div class="note-header">
-            <h1 class="py-4 text-3xl">Note: {{ $note->title }}</h1>
+            <h1 class="py-4 text-3xl">{{ $note->name }}</h1>
             <div class="note-buttons">
                 <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
                 <form action="{{ route('note.destroy', $note) }}" method="POST">
@@ -15,6 +15,12 @@
             <div class="note-body">
                 {{ $note->note }}
             </div>
+        </div>
+        <div class="note-tags">
+            <h2 class="py-4 text-2xl">Tags</h2>
+            @foreach ($note->tags as $tag)
+                <span class="tag">{{ $tag->name }}</span>
+            @endforeach
         </div>
     </div>
 </x-app-layout>
