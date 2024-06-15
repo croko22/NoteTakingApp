319cd1073ea37e0742cc16b019a4f6ef8877c0cf
croko22 <kvchambit@gmail.com>
2024-06-15 10:08:22 -0500

    feat: Add tags dropdown to note create form

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -1,10 +1,20 @@
 <x-app-layout>
     <div class="note-container single-note">
-        <h1>Create new note</h1>
+        <h1 class="py-4 text-3xl">Create new note</h1>
         <form action="{{ route('note.store') }}" method="POST" class="note">
             @csrf
             <input type="text" name="name" class="note-name" placeholder="Enter note name">
             <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here"></textarea>
+
+            <div class="note-tags">
+                <h2 class="py-4 text-2xl">Tags</h2>
+                <select name="tags[]">
+                    @foreach ($tags as $tag)
+                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
             <div class="note-buttons">
                 <a href="{{ route('note.index') }}" class="note-cancel-button">Cancel</a>
                 <button class="note-submit-button">Submit</button>
