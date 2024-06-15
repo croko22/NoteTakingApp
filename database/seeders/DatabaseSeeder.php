3b894653b64de06ab6b2a37be8ae1d8a8d6cbc2f
croko22 <kvchambit@gmail.com>
2024-06-15 12:44:06 -0500

    feat: Add Tag model and seed tags in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Note;
+use App\Models\Tag;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,13 +15,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory()->create([
-        //     'id' => 1,
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => bcrypt('password'),
-        // ]);
+        User::factory()->create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        Note::factory(10)->create();
+        // Note::factory(10)->create();
+
+        $tags = ['work', 'important'];
+        foreach ($tags as $tag) {
+
+            Tag::factory()->create([
+                'name' => $tag,
+                'user_id' => 1,
+            ])->each(function ($tag) {
+                Note::factory(10)->create()->each(function ($note) use ($tag) {
+                    $note->tags()->attach($tag);
+                });
+
+                // $tag->notes()->attach(Note::all()->random());
+            });
+        }
+
+        // Tag::factory()->create([
+        // 'name' => 'Work',
+        // 'user_id' => 1,
+        // ]);
     }
 }
